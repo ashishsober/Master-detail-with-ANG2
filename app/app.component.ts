22d5f2b36884b2ae295f26536a8a100ee581c5d0
ashishsober <ashishg@mirafra.com>
2017-01-05 19:06:51 +0530

    created two componenet hero-details and app-component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,52 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit } from '@angular/core';
+import { Hero } from './hero';
+import {HeroService} from './service/hero.service';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template:`
+            <h1>{{title}}</h1>
+            
+            <ul class="heroes">
+                <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+                  <span class="badge">{{hero.id}}</span> {{hero.name}}
+                </li>
+            </ul>
+            <hr/>
+            <h2>{{hero.name}} details!</h2>
+            <div><label>id: </label>{{hero.id}}</div>
+            <div>
+              <label>name: </label>
+              <input [(ngModel)]="hero.name" placeholder="name">
+            </div>
+            <hr/>
+            <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+
+
+            `,
+   styleUrls:["app/css/app.component.css"]
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent implements OnInit{ 
+  title = 'Angular'; 
+  selectedHero: Hero;
+  public heroes:Hero[];
+  constructor(private heroService:HeroService){ }
+  
+  //We pass our callback function as an argument to the promise's {then} method:
+  getHeroes(){
+      this.heroService.getHeroes().then(heroes => this.heroes=heroes);
+  }
+
+  ngOnInit(){
+      this.getHeroes();
+  }
+
+  onSelect(hero1: Hero): void {
+    this.selectedHero = hero1;
+  }
+  //hard coded value
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
