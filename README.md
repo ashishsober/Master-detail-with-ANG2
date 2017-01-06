1f16ada3884c7855a82aa58100c7c4e9c15f4690
ashishsober <ashishg@mirafra.com>
2017-01-06 16:44:08 +0530

    Inject angular material

--- README.md
+++ README.md
@@ -8,4 +8,12 @@ Important links
    $ https://angular.io/docs/ts/latest/tutorial/toh-pt3.html
    $ https://embed.plnkr.co/?show=preview
 
+
+#Angular Material
+ 
+ 
+[Important](https://medium.com/codingthesmartway-com-blog/using-material-design-in-angular-2-83a3128c58b7#.dd6ogmw7u)
+
+
+
    
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { MaterialModule } from '@angular/material';
+import { MdCardModule } from '@angular2-material/card';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdIconModule ,MdIconRegistry } from '@angular2-material/icon';
+
 
 
 
@@ -16,14 +21,19 @@ import { AppRoutingModule }  from './app-routing.module';
 
 @NgModule({
   imports:[ BrowserModule ,
-            FormsModule ,
-            AppRoutingModule          
+            FormsModule,
+            AppRoutingModule,
+            MdCardModule,
+            MdButtonModule,
+            MdIconModule
+           
+                
            ],
   declarations: [ AppComponent ,
                   HeroDetailComponent,
                   HeroesComponent,
                   DashboardComponent ],
-  providers: [ HeroService ],
+  providers: [ HeroService ,MdIconRegistry],
   bootstrap:    [ AppComponent ]
   
 })
