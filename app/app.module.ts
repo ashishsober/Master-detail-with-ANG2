ea79752da84e7f8288b157b451f859eb146926fa
ashishsober <ashishg@mirafra.com>
2017-01-11 15:01:25 +0530

    Created hero-detail component separately and applied angular material

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,17 +2,6 @@ import { NgModule ,ModuleWithProviders}      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
-import { MaterialModule } from '@angular/material';
-import { MdCardModule } from '@angular2-material/card';
-import { MdButtonModule } from '@angular2-material/button';
-import { MdIconModule ,MdIconRegistry } from '@angular2-material/icon';
-import { MdInputModule } from '@angular2-material/input';
-import {
-        MdRippleModule,
-        RtlModule,
-        PortalModule,
-        OverlayModule } from '@angular2-material/core';
-
 import { AppComponent }  from './app-component/app.component';
 import { HeroDetailComponent } from './hero-detail-component/hero-detail.component';
 import { HeroesComponent }     from './heroes-component/heroes.component';
--- app/hero-detail-component/hero-detail.component.ts
+++ app/hero-detail-component/hero-detail.component.ts
@@ -9,19 +9,10 @@ import { Hero } from '../hero';
 
 
 @Component({
+  moduleId: module.id,
   selector: 'my-hero-detail',
-  template: `
-              <div *ngIf="hero">
-                <h2>{{hero.name}} details!</h2>
-                <div><label>id: </label>{{hero.id}}</div>
-                <div>
-                  <label>name: </label>
-                  <input [(ngModel)]="hero.name" placeholder="name"/>
-                </div>
-                <button (click)="goBack()">Back</button>
-              </div>
-              `,
-  styleUrls:['app/hero-detail-component/hero-detail.component.css'],
+  templateUrl:'hero-detail.component.html',
+  styleUrls:['hero-detail.component.css'],
 })
 export class HeroDetailComponent implements OnInit{
     @Input() hero: Hero;
