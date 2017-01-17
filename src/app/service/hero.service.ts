702d243d09994cc3cd01f695e43722acefe4999d
ashishsober <ashishg@mirafra.com>
2017-01-17 16:25:01 +0530

    injected observales in hero.service

--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -11,21 +11,15 @@ import 'rxjs/add/operator/catch';
 @Injectable()
 export class HeroService {
 
-  //Observable = Observable.range(0, 10);
-// myObservable.subscribe(
-//   data => {
-//     console.log(data);  
-//   }
 //use promisess here to call asynchronous call,If the data is coming from the remote server
 //so that over code will not get blocked. for the waiting of the respond from the server
 
   constructor(){
     let myObservable = Observable.range(0,10);
     myObservable.subscribe(
-    data=>{
-      console.log(data);
-    }
-      
+        data=>{
+          console.log(data);
+        }
     )
   }
 
