33b658e46c1d16000ef31f4febe0c5ecee15cdef
ashishsober <ashishg@mirafra.com>
2017-01-20 14:10:15 +0530

    dashboard component working with th id

--- src/app/dashboard-component/dashboard.component.ts
+++ src/app/dashboard-component/dashboard.component.ts
@@ -17,7 +17,7 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+      .subscribe(data => this.heroes = data.slice(1, 5));
   }
 
   // public direction = "row";
