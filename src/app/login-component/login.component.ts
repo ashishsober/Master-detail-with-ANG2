867dfa0a7daf662449caf6b5574ce5050c00c00d
ashishsober <ashishg@mirafra.com>
2017-02-10 19:34:35 +0530

    corrected some css on content area Signed-off-by: ashishsober <ashishg@mirafra.com>

--- src/app/login-component/login.component.ts
+++ src/app/login-component/login.component.ts
@@ -11,12 +11,17 @@ export class LoginComponent {
   user={username:'',password :''};
   myPhotoUrl:string;
   get myContent() { return JSON.stringify(this.user); }
+  
   constructor(private firebase_login_service:FirebaseLoginService) {}
   
   onSubmit() {
-    //console.log("my console" +this.user);
+    console.log("my console" +this.user);
+  }
+
+
+  googleLogin() {
+    console.log("i m inside google Login method");
     this.firebase_login_service.getAuth()
                                .then(result => this.myPhotoUrl = result.user.photoURL);
   }
-
  }
