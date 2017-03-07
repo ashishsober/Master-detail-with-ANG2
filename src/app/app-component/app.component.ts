04a3268cc06f5ee8836cfa921e0817a132e8dda2
ashishsober <ashishg@mirafra.com>
2017-03-07 16:57:32 +0530

    css changes  3

--- src/app/app-component/app.component.ts
+++ src/app/app-component/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+//import {AuthService} from '../service/auth-service';
 @Component({
   moduleId:'module.id',
   selector: 'app-root',
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  signInOut :string ='Login';
+
+  constructor(){}
+  loginOrOut(){
+    //const isAuthenticated = this.authservice.isAuthenticated;
+
+  }
 }
--- src/app/hero.ts
+++ src/app/hero.ts
@@ -2,3 +2,8 @@ export class Hero {
   id:number;
   name:string;
 }
+
+export interface IUserLogin {
+    email: string;
+    password: string;
+}
