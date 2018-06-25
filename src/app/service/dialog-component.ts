41a3f67efd93542b097050d9d758c9076d7345e0
Ashish Gupta <ashish.gupta1@sc.com>
2018-06-25 16:32:32 +0530

    cancel button on modal functionality fixed

--- src/app/service/dialog-component.ts
+++ src/app/service/dialog-component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
     template: `
       
@@ -7,11 +7,17 @@ import { Component } from '@angular/core';
           <h2 class="mat-h2">Activation blocked for you ,Please do login</h2>
       </mat-dialog-content>
       <mat-dialog-actions>
-        <button mat-button type="button" mat-dialog-close mat-raised-button color="warn">Cancel</button>
+        <button mat-button type="button" mat-dialog-close mat-raised-button color="warn" (click)="cancel()">Cancel</button>
       </mat-dialog-actions>
     `
   })
-  export class DialogComponent {}
+  export class DialogComponent {
+    constructor(private router:Router){}
+    cancel(){
+      this.router.navigate(['login']);
+    }
+
+  }
 
 
 
