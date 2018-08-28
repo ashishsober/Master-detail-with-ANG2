2c8426094c3b605a7bb806eb0500de3bdfec9a4c
Ashish Gupta <ashish.gupta1@sc.com>
2018-08-28 14:03:13 +0530

    select-box component change

--- src/client/app/@themes/components/select-box/select-box.component.ts
+++ src/client/app/@themes/components/select-box/select-box.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit, Output, Input } from '@angular/core';
-
+import { Hero } from '../../../core/interface';
+import { DataService } from '../../../core/data.service';
 
 @Component({
     moduleId: 'module.id',
     selector: 'select-box',
-    templateUrl:'./select-box.component.html'
+    templateUrl: './select-box.component.html'
 })
 export class SelectBox {
-    @Input()data;
+    @Input() data;
+    heroes: Hero[] = [];
+    constructor(private heroService: DataService) { }
+    
+    lovData(lovFieldData: string) {
+        this.heroService.getHeroes()
+            .then(heroes => this.heroes = heroes.slice(1, 5));
+    }
 }
--- src/client/app/pages/components/applicant-form-component/applicant-field.component.ts
+++ src/client/app/pages/components/applicant-form-component/applicant-field.component.ts
@@ -1,44 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../../core/data.service';
-import { Hero } from '../../../core/interface';
+
 
 @Component({
   moduleId: 'module.id',
   selector: 'my-material',
   templateUrl: './applicant-field.component.html',
-  styleUrls:['./applicant-field.component.scss']
-  
+  styleUrls: ['./applicant-field.component.scss']
+
 })
 export class ApplicantFieldComponent implements OnInit {
-
-  constructor(private deroService: DataService) { }
   mydata;
-  heroes: Hero[] = [];
-  count=[];
+  count = [];
+
+  constructor(private heroService: DataService) { }
+
   ngOnInit() {
-    this.deroService.getFieldmetadata()
+    this.heroService.getFieldmetadata()
       .then(data => {
         this.mydata = data.fieldmetadata.data.stages[0].fields;
         //console.log("my fieldmetadata===" + this.mydata);
       });
   }
 
-  lovData() {
-    this.deroService.getHeroes()
-      .then(heroes => this.heroes = heroes.slice(1, 5));
-  }
-
-  getDynamicClass(rwb_name,seqNo, positioning, i) {
-    console.log('Rwb_name--->'+rwb_name+'  sequence_no----->'+seqNo+'  positioning--->'+ positioning+'  my index----->'+ i);
+  getDynamicClass(rwb_name, seqNo, positioning, i) {
+    //console.log('Rwb_name--->' + rwb_name + '  sequence_no----->' + seqNo + '  positioning--->' + positioning + '  my index----->' + i);
     this.count.push(i);
     if (this.count.length === 1 && positioning === '2') {
-        return 'left-border half-width';
+      return 'left-border half-width';
     } else if (positioning === '2') {
       this.count = [];
-        return 'no-border right-border half-width';
+      return 'no-border right-border half-width';
     } else {
       this.count = [];
-        return 'full-width';
+      return 'full-width';
     }
   }
 }
