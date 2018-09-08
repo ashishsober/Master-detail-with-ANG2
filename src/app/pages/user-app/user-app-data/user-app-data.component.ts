9b2de1b6de8cca03bbae4beab728d17adb825bc3
Ashish Gupta <ashish.gupta1@sc.com>
2018-09-09 03:12:28 +0530

    changed the file name of userdata component

--- src/app/pages/user-app/user-app-data/user-app-data.component.ts
+++ src/app/pages/user-app/user-app-data/user-app-data.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: 'module.id',
   selector: 'user-app-data-root',
-  templateUrl: './user-app-data.html'
+  templateUrl: './user-app-data.component.html'
 })
 export class UserAppDataComponent{
   constructor(){}
