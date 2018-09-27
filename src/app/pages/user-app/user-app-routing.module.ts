e692d5a3b12f28969281da2ff89e52db3fbe5514
Ashish Gupta <ashish.gupta1@sc.com>
2018-09-27 18:46:36 +0530

    removed unrelated component

--- src/app/pages/user-app/user-app-routing.module.ts
+++ src/app/pages/user-app/user-app-routing.module.ts
@@ -4,11 +4,7 @@ import { UserAppComponent } from './user-app.component';
 import { UserAppDataComponent } from './user-app-data/user-app-data.component';
 import { ProfileComponent } from './profiles/profile.component';
 import { LocationComponent } from './location-component/location.component';
-import { BoardComponent } from './location-component/board-component/board.component';
-import { BoardMessageComponent } from './location-component/board-message/board-message.component';
-import { GeneralComponent } from './location-component/general-component/general.component';
-import { PotViewComponent } from './location-component/pot-view-component/pot-view.component';
-import { PlayerComponent } from './location-component/player-component/player.component';
+
 const userAppRoutes: Routes = [
   {
     path: '', component: UserAppComponent,/** router outlet is here */
